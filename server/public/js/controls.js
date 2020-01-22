8d091fa04a06f9a1ea7d7105a2d4ae59cb92ec7a
Federico Jure <fjure@atixlabs.com>
2020-01-22 19:07:48 -0300

    add mobile input for left, right and jump

--- server/public/js/controls.js
+++ server/public/js/controls.js
@@ -1,15 +1,43 @@
 let controls = null;
 
-const initControls = (input) => {
-    controls = input.keyboard.addKeys({
-        up: "W",
-        down: "S",
-        left: "A",
-        right: "D",
-        jump: "SPACE",
-        attack1: "U",
-        attack2: "I",
-        attack3: "O",
-        lag: "L"
-      });
-}
+const initControls = input => {
+  const isMobile = typeof window.orientation !== "undefined";
+
+  controls = input.keyboard.addKeys({
+    up: "W",
+    down: "S",
+    left: "A",
+    right: "D",
+    jump: "SPACE",
+    attack1: "U",
+    attack2: "I",
+    attack3: "O",
+    lag: "L"
+  });
+
+  if (true) {
+    const leftButton = document.getElementById("leftButton");
+    const rightButton = document.getElementById("rightButton");
+    const jumpButton = document.getElementById("jumpButton");
+    leftButton.onmousedown = ev => {
+      controls.left.isDown = true;
+    }
+    leftButton.onmouseup = ev => {
+      controls.left.isDown = false;
+    }
+    rightButton.onmousedown = ev => {
+      controls.right.isDown = true;
+    }
+    rightButton.onmouseup = ev => {
+      controls.right.isDown = false;
+    }
+    jumpButton.onmousedown = ev => {
+      controls.jump.isDown = true;
+    }
+    jumpButton.onmouseup = ev => {
+      controls.jump.isDown = false;
+    }
+  } else {
+
+  }
+};
